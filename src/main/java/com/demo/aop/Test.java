0d0a343b6a37cdadcd8378598d9901cbc82e1346
tong <1918939227@qq.com>
2019-03-24 16:26:38 +0800

    third modify

--- src/main/java/com/demo/aop/Test.java
+++ src/main/java/com/demo/aop/Test.java
@@ -1,12 +1,19 @@
 package com.demo.aop;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * @author zhangwei
  * @version V1.0
- * @Title: com.demo.aop
+ * @Title: com.demo.aopgi
  * @Package SpringAOP
  * @Description: TODO
  * @date 2017-10-18 14:12
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+
+        System.out.println("this is my name");
+    }
 }
